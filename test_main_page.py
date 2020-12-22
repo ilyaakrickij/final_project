8a1bdd7381c70fb403b417a6ea09dd357ad08479
Акрицкий Илья Александрович <Ilya.Akriczky@itech.ru>
2020-12-22 12:33:45 +0300

    Добавлены 3 проверки: проверка url, проверка формы логина, формы регистрации

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,7 @@
 from .page.main_page import MainPage
+from .page.login_page import LoginPage
+
+
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -6,6 +9,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    login = LoginPage(browser, browser.current_url)
+    login.should_be_login_page()
+
     
     
 def test_guest_should_see_login_link(browser):
